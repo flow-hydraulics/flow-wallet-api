eb09a53c8f895aea70f619f11527a7d87d1738e0
Lauri Junkkari <latenssi@gmail.com>
2021-05-05 10:55:10 +0300

    Refactor account service tests

---
+++ account/test_helpers.go
@@ -0,0 +1,58 @@
+package account
+
+import (
+	"log"
+	"os"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/eqlabs/flow-wallet-service/data/gorm"
+	"github.com/eqlabs/flow-wallet-service/keys/simple"
+	"github.com/joho/godotenv"
+	"github.com/onflow/flow-go-sdk/client"
+	"google.golang.org/grpc"
+)
+
+type testConfig struct {
+	FlowGateway string `env:"FLOW_GATEWAY,required"`
+}
+
+const testDbDSN = "test.db"
+const testDbType = "sqlite"
+
+func SetupTestService(l *log.Logger) (result *Service, err error) {
+	godotenv.Load("../.env.test")
+
+	os.Setenv("DB_DSN", testDbDSN)
+	os.Setenv("DB_TYPE", testDbType)
+
+	var cfg testConfig
+	if err = env.Parse(&cfg); err != nil {
+		return
+	}
+
+	// Flow client
+	fc, err := client.New(cfg.FlowGateway, grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+
+	// Database
+	db, err := gorm.NewStore(l)
+	if err != nil {
+		return
+	}
+
+	// Key manager
+	km, err := simple.NewKeyManager(l, db, fc)
+	if err != nil {
+		return
+	}
+
+	result = NewService(l, db, km, fc)
+
+	return
+}
+
+func TeardDownTestService() {
+	os.Remove(testDbDSN)
+}
