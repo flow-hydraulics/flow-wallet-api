50aa40b3cb59f259876513a993eed20a29d208f7
Lauri Junkkari <latenssi@gmail.com>
2021-08-20 13:31:39 +0300

    Add FLOW_WALLET prefix to all env variables, resolves #138, breaking changes

--- configs/configs_test.go
+++ configs/configs_test.go
@@ -1,28 +1,37 @@
 package configs
 
 import (
+	"os"
 	"testing"
 )
 
 func TestParseConfig(t *testing.T) {
-	t.Run("Read environment variables from file", func(t *testing.T) {
-		opts := &Options{EnvFilePath: ".testfile"}
-		cfg, err := ParseConfig(opts)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if cfg.AdminAddress != "admin-address" {
-			t.Errorf(`expected "admin-address", got "%s"`, cfg.AdminAddress)
-		}
-	})
+	os.Clearenv()
+	os.Setenv("FLOW_WALLET_ENABLED_TOKENS", "FlowToken:0x0ae53cb6e3f42a79:flowToken,FUSD:0xf8d6e0586b0a20c7:fusd")
+	defer os.Clearenv()
 
-	t.Run("", func(t *testing.T) {
-		cfg, err := ParseConfig(nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if cfg.AdminAddress != "admin-address" {
-			t.Errorf(`expected "admin-address", got "%s"`, cfg.AdminAddress)
-		}
-	})
+	opts := &Options{EnvFilePath: ".testfile"}
+	cfg, err := ParseConfig(opts)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if cfg.AdminAddress != "admin-address" {
+		t.Errorf(`expected "AdminAddress" to equal "admin-address", got "%s"`, cfg.AdminAddress)
+	}
+
+	if cfg.WorkerCount != 100 {
+		t.Errorf(`expected "WorkerCount" to equal 100, got %d`, cfg.WorkerCount)
+	}
+
+	if len(cfg.EnabledTokens) != 2 ||
+		cfg.EnabledTokens[0] != "FlowToken:0x0ae53cb6e3f42a79:flowToken" ||
+		cfg.EnabledTokens[1] != "FUSD:0xf8d6e0586b0a20c7:fusd" {
+		t.Errorf(
+			"expected %#v, got %#v",
+			[]string{"FlowToken:0x0ae53cb6e3f42a79:flowToken", "FUSD:0xf8d6e0586b0a20c7:fusd"},
+			cfg.EnabledTokens,
+		)
+	}
 }
