20f714196675c2ec700bec3e492f9a77c75e0f65
Lauri Junkkari <latenssi@gmail.com>
2021-09-01 10:31:56 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 now=$(date +'%Y-%m-%d_%T')
 go build -a -ldflags "-linkmode external -extldflags '-static' -s -w -X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"  -o main main.go
