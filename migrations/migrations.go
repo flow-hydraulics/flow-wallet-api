643aa7fbc8cabc91c4df6fd7f708c31f63023508
Klaus Dahlén <nanuuki@users.noreply.github.com>
2021-11-19 09:53:40 +0200

    Fix linter warning "redundant type from array"

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -10,22 +10,22 @@ import (
 
 func List() []*gormigrate.Migration {
 	ms := []*gormigrate.Migration{
-		&gormigrate.Migration{
+		{
 			ID:       m20210922.ID,
 			Migrate:  m20210922.Migrate,
 			Rollback: m20210922.Rollback,
 		},
-		&gormigrate.Migration{
+		{
 			ID:       m20211005.ID,
 			Migrate:  m20211005.Migrate,
 			Rollback: m20211005.Rollback,
 		},
-		&gormigrate.Migration{
+		{
 			ID:       m20211015.ID,
 			Migrate:  m20211015.Migrate,
 			Rollback: m20211015.Rollback,
 		},
-		&gormigrate.Migration{
+		{
 			ID:       m20211118.ID,
 			Migrate:  m20211118.Migrate,
 			Rollback: m20211118.Rollback,
