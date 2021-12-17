fe6bae334c597cd56785de0d49462dea79c03c87
Lauri Junkkari <latenssi@gmail.com>
2021-12-17 10:22:32 +0200

    Update concurrent account create testcase

--- tests/accounts_service_test.go
+++ tests/accounts_service_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/flow-hydraulics/flow-wallet-api/accounts"
 	"github.com/flow-hydraulics/flow-wallet-api/jobs"
 	"github.com/flow-hydraulics/flow-wallet-api/tests/internal/test"
 )
@@ -116,30 +117,40 @@ func Test_Delete_Non_Custodial_Account_Is_Idempotent(t *testing.T) {
 	}
 }
 
-// Test if the service is able to simultaneously create multiple accounts
+// Test if the service is able to concurrently create multiple accounts
 func Test_Add_Multiple_New_Custodial_Accounts(t *testing.T) {
 	cfg := test.LoadConfig(t, testConfigPath)
-	svc := test.GetServices(t, cfg).GetAccounts()
+
+	instanceCount := 1
+	accountsToCreate := instanceCount * 5
+	// Worst case scenario where theoretically maximum number of transactions are done concurrently
+	cfg.WorkerCount = uint(accountsToCreate / instanceCount)
+
+	svcs := make([]*accounts.Service, instanceCount)
+
+	for i := 0; i < instanceCount; i++ {
+		svcs[i] = test.GetServices(t, cfg).GetAccounts()
+	}
 
 	if cfg.AdminProposalKeyCount <= 1 {
 		t.Skip("skipped as \"cfg.AdminProposalKeyCount\" is less than or equal to 1")
 	}
 
-	if acccounts, err := svc.List(0, 0); err != nil {
+	if acccounts, err := svcs[0].List(0, 0); err != nil {
 		t.Fatal(err)
 	} else if len(acccounts) > 1 {
 		t.Fatal("expected there to be only 1 account")
 	}
-
-	accountsToCreate := 3
 
 	wg := sync.WaitGroup{}
 	errChan := make(chan error, accountsToCreate*3)
 
 	for i := 0; i < accountsToCreate; i++ {
 		wg.Add(1)
-		go func() {
+		go func(i int, svcs []*accounts.Service) {
 			defer wg.Done()
+
+			svc := svcs[i%instanceCount]
 
 			job, _, err := svc.Create(context.Background(), false)
 			if err != nil {
@@ -151,7 +162,7 @@ func Test_Add_Multiple_New_Custodial_Accounts(t *testing.T) {
 				time.Sleep(100 * time.Millisecond)
 			}
 
-			if job.State == jobs.Error {
+			if job.State == jobs.Error || job.State == jobs.Failed {
 				errChan <- fmt.Errorf(job.Error)
 				return
 			}
@@ -160,7 +171,7 @@ func Test_Add_Multiple_New_Custodial_Accounts(t *testing.T) {
 				errChan <- err
 				return
 			}
-		}()
+		}(i, svcs)
 	}
 
 	wg.Wait()
@@ -171,7 +182,7 @@ func Test_Add_Multiple_New_Custodial_Accounts(t *testing.T) {
 	default:
 	}
 
-	if acccounts, err := svc.List(0, 0); err != nil {
+	if acccounts, err := svcs[0].List(0, 0); err != nil {
 		t.Fatal(err)
 	} else if len(acccounts) < 1+accountsToCreate {
 		t.Fatalf("expected there to be %d accounts", 1+accountsToCreate)
